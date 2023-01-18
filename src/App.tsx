d46ce451740242a917c51c597828277146844f9a
edocai <edocaai@hotmail.com>
2023-01-18 14:54:57 +0300

    handle change function

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,20 @@ function App() {
   const [task, setTask] = useState<string>("")
   const [deadline, setDeadline] = useState<number>(0)
 
-
+  function handleChange(event: ChangeEvent<HTMLInputElement>): void {
+    if(event.target.name === "task") {
+      setTask(event.target.value)
+    } else {
+      setDeadline(Number(event.target.value))
+    }
+  }
 
   return (
     <div className="App">
       <div className="header">
         <div className="input-container">
-          <input type="text" placeholder='Task' />
-          <input type="number" placeholder='Deadline' />
+          <input type="text" placeholder='Task' name='task' onChange={handleChange} />
+          <input type="number" placeholder='Deadline' name='deadline' onChange={handleChange} />
         </div>
         <button>Add Task</button>
       </div>
