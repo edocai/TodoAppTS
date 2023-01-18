e1c97cdb865a82c48bd9e77340187776b3d79c50
edocai <edocaai@hotmail.com>
2023-01-18 14:50:48 +0300

    adding states

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
+import { useState, ChangeEvent } from 'react'
 import './App.css'
 
 function App() {
+  const [task, setTask] = useState<string>("")
+  const [deadline, setDeadline] = useState<number>(0)
+
 
 
   return (
     <div className="App">
-
+      <div className="header">
+        <div className="input-container">
+          <input type="text" placeholder='Task' />
+          <input type="number" placeholder='Deadline' />
+        </div>
+        <button>Add Task</button>
+      </div>
+      <div className="todo-list"></div>
     </div>
   )
 }
